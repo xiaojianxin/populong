e066789de4e1a8990aaf3a0e79c214662c5dd0de
chang <335383263@qq.com>
2015-06-06 20:42:45 +0800

    xiugai

---
+++ fans.php
@@ -0,0 +1,309 @@
+<div id="self_fans" class="container-fluid">
+<div class="row">
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">lisisi</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                             <span class="btn btn-default foucus">
+                                                <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                                     <ul class="dropdown-menu">
+                                                         <li>
+                                                             <a href="#">取消关注</a>
+                                                         </li>
+
+                                                     </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="row">
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                             <span class="btn btn-default foucus">
+                                                <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                                     <ul class="dropdown-menu">
+                                                         <li>
+                                                             <a href="#">取消关注</a>
+                                                         </li>
+
+                                                     </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                             <span class="btn btn-default foucus">
+                                                <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                                     <ul class="dropdown-menu">
+                                                         <li>
+                                                             <a href="#">取消关注</a>
+                                                         </li>
+
+                                                     </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                             <span class="btn btn-default foucus">
+                                                <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                                     <ul class="dropdown-menu">
+                                                         <li>
+                                                             <a href="#">取消关注</a>
+                                                         </li>
+
+                                                     </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-4 fans_card">
+        <div class="row">
+            <div class="col-xs-4 fans_pic">
+                <a href="./others.html" class="fans_pic">
+                    <img class="fan" src="./img/fan_01.png" alt="" />
+                </a>
+            </div>
+
+            <div class="col-xs-6 fans_description">
+                <div class="fans_name">张三三</div>
+                <div class="fans_introduction">某公司老总</div>
+                <div class="fans_introduction">关注11|粉丝20万</div>
+                                            <span class="btn btn-default foucus">
+                                            <span class="dropdown-toggle" data-toggle="dropdown" >互相关注<span class="glyphicon glyphicon-chevron-up"></span></span>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="#">取消关注</a>
+                                                </li>
+
+                                            </ul>
+                                            </span>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="seemore"><a>查看更多</a></div>
+</div>
--- help.php
+++ help.php
@@ -20,6 +20,11 @@ session_start();
     <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="./js/swiper3.07.min.js"></script>
     <script type="text/javascript" src="./js/jPages.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="http://apps.bdimg.com/libs/html5shiv/3.7/html5shiv.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 <?php require('./nav.php') ?>
