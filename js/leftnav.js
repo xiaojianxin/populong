510785987042d85a9ec4502d26098111ad325bdc
chang <335383263@qq.com>
2015-06-06 22:44:17 +0800

    xiugai

--- js/leftnav.js
+++ js/leftnav.js
@@ -1,7 +1,9 @@
   $(function() {
-        $('.nav li').click(function(e) {
+        $('.nav li').click(function(e){
             $('.nav li').removeClass('active');
+            //alert('111');
             //$(e.target).addClass('active');
             $(this).addClass('active');
         });
+
     });
