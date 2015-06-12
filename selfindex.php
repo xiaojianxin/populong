a5f9b74d8d00a46de021068b9f276e7763f6329d
xiaojianxin <335383263@qq.com>
2015-06-12 15:12:59 +0800

    xiugai

--- selfindex.php
+++ selfindex.php
@@ -5,6 +5,16 @@
 
                     </div>
                     <div class="self_explain">Lilith-泡泡隆设计师</div>
+                    <div class='row'>
+                        <div class="col-xs-4" style="width:37%"></div>
+                        <a href="#" id="popover_bottom1" class='col-xs-2' style="width:15%" rel="popover" data-content="It's so simple to create a tooltop for my website!">发起者 lv3</a>
+                        <a href="#" id="popover_bottom2" class='col-xs-2' rel="popover" data-content="It's so simple to create a tooltop for my website!">投资者 lv6</a>
+                        <div class="col-xs-4"></div>
+
+                    </div>
+                  
+                    
+                   
                     <div class="container-fluid" id="self_nav">
                         <div id="topbottom" class="row">
                             <div id="toChooseone" class="col-xs-3 content_nav" ><a>关注者110</div>
@@ -17,4 +27,7 @@
                         <div class="tab-pane active" id="fans"><?php require('./fans.php') ?></div>
                         <div class="tab-pane" id="concern"><?php require('./concern.php') ?></div>
                     </div>
+
+<script>
+
 
