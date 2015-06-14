05875f795ecd376611ad79980e95a1b3ddc548b5
jiye <jiye@neotel.com.cn>
2015-06-14 22:26:15 +0800

    up

--- cardlist.php
+++ cardlist.php
@@ -1,3 +1,23 @@
+
+
+<div id="deleteCardModal" class="modal fade" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="myModalLabel">
+            标题栏
+        </h3>
+    </div>
+    <div class="modal-body">
+        <p>
+            显示信息
+        </p>
+    </div>
+    <div class="modal-footer">
+        <button class="btn" data-dismiss="modal" aria-hidden="true">关闭</button>
+        <button class="btn btn-primary">保存设置</button>
+    </div>
+</div>
+
 <div id="cardlist" class="container">
     <div class="cardTitle">
         <h2>银行卡绑定</h2>
@@ -18,7 +38,7 @@
                             <div class="divider"></div>
                             <div class="actionBtn">
                                 <span class="btn">修改</span>
-                                <span class="btn">删除</span>
+                                <span class="btn" id="deleteCard"><a href="#deleteCardModal" role="button" class="btn" data-toggle="modal">删除</a></span>
                             </div>
                         </div>
                     </div>
