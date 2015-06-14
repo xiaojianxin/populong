75e4424bd4af10e7fcd9db5cc6558417963860d0
chang <335383263@qq.com>
2015-06-14 16:46:44 +0800

    zengjia

--- safe.php
+++ safe.php
@@ -42,13 +42,42 @@
 				33*******@qq.com
 			</div>
 			<div class="col-xs-2">
-				<a>修改</a>
+				<a id="edit-email">修改</a>
 			</div>
-			<div class='row edit'>
-				<div class='col-xs-1'></div>
-				<div class="col-xs-2">已绑定手机号</div>
-				<div class="col-xs-2">13*********</div>
+			<div class='tianchong' style="height:40px;"></div>
+			<div class='row edit edit-email hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2"><div class='edit-title'>已绑定邮箱</div></div>
+				<div class="col-xs-4">33*******@qq.com</div>
 			</div>
+			<form action='' method="post">
+			<div class='row edit edit-email hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2 edit-title"><div class='edit-title'><span class="glyphicon glyphicon-star warning"></span>&nbsp已绑定邮箱</div></div>
+				<div class="col-xs-4">
+					<input class='form-control' placeholder="请输入邮箱"/>
+				</div>
+				<div class="col-xs-4">
+					<img class="pic-password" src="./img/regist_05.png" alt="" />
+					<a class="refresh">刷新</a>
+				</div>
+			</div>
+			<div class='row edit edit-email2 hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2 edit-title"><div class='edit-title'><span class="glyphicon glyphicon-star warning"></span>&nbsp已绑定邮箱</div></div>
+				<div class="col-xs-4">
+					<input class='form-control' placeholder="请输入邮箱"/>
+				</div>
+			</div>
+			<div class='row edit edit-email hidden'>
+				<div class='col-xs-4'></div>
+				<div class="col-xs-4"><a class='btn btn-success to-email2' style="color:white;">发送验证邮件</a></div>
+			</div>
+			<div class='row edit edit-email2 hidden'>
+				<div class='col-xs-4'></div>
+				<div class="col-xs-4"><input type='submit' class='btn btn-success' value='下一步' /></div>
+			</div>
+		</form>
 		</div>
 		<div class='row safe-content-detail'>
 			<div class='col-xs-1'>
@@ -61,8 +90,37 @@
 				133********
 			</div>
 			<div class="col-xs-2">
-				<a>修改</a>
+				<a id="edit-telephone">修改</a>
 			</div>
+			<div class='tianchong' style="height:40px;"></div>
+			<div class='row edit-telephone hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2"><div class='edit-title'>已绑定手机</div></div>
+				<div class="col-xs-4">13*******</div>
+			</div>
+			<form action='' method='post'>
+			<div class='row edit edit-telephone hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2 edit-title"><div class='edit-title'><span class="glyphicon glyphicon-star warning"></span>&nbsp手机验证码</div></div>
+				<div class="col-xs-4">
+					<input class='form-control' placeholder="请输入验证码"/>
+				</div>
+				<div class="col-xs-4">
+					<a class='btn btn-success' style="color:white; float:left">点击获取验证码</a>
+				</div>
+			</div>
+			<div class='row edit edit-telephone hidden'>
+				<div class='col-xs-2'></div>
+				<div class="col-xs-2 edit-title"><div class='edit-title'><span class="glyphicon glyphicon-star warning"></span>&nbsp;填写新手机</div></div>
+				<div class="col-xs-4">
+					<input class='form-control' placeholder="请输入手机号"/>
+				</div>
+			</div>
+			<div class='row edit edit-telephone hidden'>
+				<div class='col-xs-4'></div>
+				<div class="col-xs-4"><input type='submit' class='btn btn-success' value='下一步' /></div>
+			</div>
+			</form>
 		</div>
 		<div class='row safe-content-detail'>
 			<div class='col-xs-1'>
@@ -94,3 +152,38 @@
 		</div>
 	</div>
 </div>
+<script>
+var judge = 0;
+$('#edit-email').click(function(){
+	
+	if (judge == 0)
+	{
+		$(this).html('取消修改');
+		$('.edit-email').removeClass('hidden');
+		judge = 1;
+	}else if(judge == 1) {
+		$(this).html('修改');
+		$('.edit-email').addClass('hidden');
+		judge = 0;
+	};
+});
+$('#edit-telephone').click(function(){
+	
+	if (judge == 0)
+	{
+		$(this).html('取消修改');
+		$('.edit-telephone').removeClass('hidden');
+		judge = 1;
+	}else if(judge == 1) {
+		$(this).html('修改');
+		$('.edit-telephone').addClass('hidden');
+		judge = 0;
+	};
+});
+$('.to-email2').click(function(){
+	
+		$('.edit-email').addClass('hidden');
+		$('.edit-email2').removeClass('hidden');
+	
+});
+</script>
