db941d6ecd4315e8b2e9ff2b33077656863389d9
chang <335383263@qq.com>
2015-07-05 19:58:16 +0800

    xiugai

--- dealrecord.php
+++ dealrecord.php
@@ -24,11 +24,11 @@
                
                 <div class="row dealtype">
                     <span>&nbsp;&nbsp;交易类型:&nbsp;</span>
-                    <span class="active">&nbsp;全部&nbsp;</span>
-                    <span>&nbsp;充值&nbsp;</span>
-                    <span>&nbsp;提现&nbsp;</span>
-                    <span >&nbsp;投资&nbsp;</span>
-                    <span >&nbsp;收益&nbsp;</span>
+                    <span class="active showAll">&nbsp;全部&nbsp;</span>
+                    <span class="showRecharge">&nbsp;充值&nbsp;</span>
+                    <span class="showEncash">&nbsp;提现&nbsp;</span>
+                    <span class="showInvest">&nbsp;投资&nbsp;</span>
+                    <span class="showGain">&nbsp;收益&nbsp;</span>
                 </div>
 
                 
@@ -43,7 +43,7 @@
                                 <th>备注</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="tbody1">
 
                         <?php foreach ($result as $dealcontent) { ?>    
                         <tr class="warning">
@@ -56,6 +56,74 @@
                             <td><?php echo $dealcontent->comment ?></td>
                         </tr>
                         <?php } ?>
+
+                        </tbody>
+                        <tbody class="tbody2" style="display: none;">
+
+                        <?php foreach ($result as $dealcontent)
+                        if($dealcontent->tradeType=="充值")
+                        { ?>
+                            <tr class="warning">
+                                <td><?php $time = $dealcontent->time ;
+                                    echo date('Y-m-d H:i:s',$time)
+                                    ?></td>
+                                <td> <?php echo $dealcontent->tradeType ?></td>
+                                <td><a href="./pro.php?id=<?php echo $dealcontent->amount?>"><?php echo $dealcontent->projName ?></a></td>
+                                <td style="color: #6cc87f"> <?php echo $dealcontent->amount;?></td>
+                                <td><?php echo $dealcontent->comment ?></td>
+                            </tr>
+                        <?php } ?>
+
+                        </tbody>
+                        <tbody class="tbody3" style="display: none;">
+
+                        <?php foreach ($result as $dealcontent)
+                            if($dealcontent->tradeType=="提现")
+                            { ?>
+                                <tr class="warning">
+                                    <td><?php $time = $dealcontent->time ;
+                                        echo date('Y-m-d H:i:s',$time)
+                                        ?></td>
+                                    <td> <?php echo $dealcontent->tradeType ?></td>
+                                    <td><a href="./pro.php?id=<?php echo $dealcontent->amount?>"><?php echo $dealcontent->projName ?></a></td>
+                                    <td style="color: #6cc87f"> <?php echo $dealcontent->amount;?></td>
+                                    <td><?php echo $dealcontent->comment ?></td>
+                                </tr>
+                            <?php } ?>
+
+                        </tbody>
+                        <tbody class="tbody4" style="display: none;">
+
+                        <?php foreach ($result as $dealcontent)
+                            if($dealcontent->tradeType=="投资")
+                            { ?>
+                                <tr class="warning">
+                                    <td><?php $time = $dealcontent->time ;
+                                        echo date('Y-m-d H:i:s',$time)
+                                        ?></td>
+                                    <td> <?php echo $dealcontent->tradeType ?></td>
+                                    <td><a href="./pro.php?id=<?php echo $dealcontent->amount?>"><?php echo $dealcontent->projName ?></a></td>
+                                    <td style="color: #6cc87f"> <?php echo $dealcontent->amount;?></td>
+                                    <td><?php echo $dealcontent->comment ?></td>
+                                </tr>
+                            <?php } ?>
+
+                        </tbody>
+                        <tbody class="tbody5" style="display: none;">
+
+                        <?php foreach ($result as $dealcontent)
+                            if($dealcontent->tradeType=="收益")
+                            { ?>
+                                <tr class="warning">
+                                    <td><?php $time = $dealcontent->time ;
+                                        echo date('Y-m-d H:i:s',$time)
+                                        ?></td>
+                                    <td> <?php echo $dealcontent->tradeType ?></td>
+                                    <td><a href="./pro.php?id=<?php echo $dealcontent->amount?>"><?php echo $dealcontent->projName ?></a></td>
+                                    <td style="color: #6cc87f"> <?php echo $dealcontent->amount;?></td>
+                                    <td><?php echo $dealcontent->comment ?></td>
+                                </tr>
+                            <?php } ?>
 
                         </tbody>
                     </table>
@@ -125,24 +193,7 @@
                         </tbody>
                     </table>
                 </div>
-                <div id="pagination">
-                    <div class="holder" style="text-align: center;">
-                        <a class="jp-previous jp-disabled">←</a>
-                        <a class="jp-current">1</a>
-                        <span class="jp-hidden">...</span>
-                        <a>2</a>
-                        <a>3</a>
-                        <a>4</a>
-                        <a>5</a>
-                        <a class="jp-hidden">6</a>
-                        <a class="jp-hidden">7</a>
-                        <a class="jp-hidden">8</a>
-                        <a class="jp-hidden">9</a>
-                        <span>...</span>
-                        <a>10</a>
-                        <a class="jp-next">→</a>
-                    </div>
-                </div>
+
             </div>
 
 <script type="text/javascript">
@@ -151,4 +202,64 @@
     //         containerID : "deal-list"
     //     });
     // });
+    $(".showAll").click(function(){
+        $(".dealtype span").each(function(){
+            $(this).removeClass("active")
+        });
+
+        $(".showAll").addClass("active");
+        $(".tbody2").hide();
+        $(".tbody3").hide();
+        $(".tbody4").hide();
+        $(".tbody5").hide();
+        $(".tbody1").show();
+    });
+    $(".showRecharge").click(function(){
+        $(".dealtype span").each(function(){
+            $(this).removeClass("active")
+        });
+
+        $(".showRecharge").addClass("active");
+        $(".tbody1").hide();
+        $(".tbody3").hide();
+        $(".tbody4").hide();
+        $(".tbody5").hide();
+        $(".tbody2").show();
+    });
+    $(".showEncash").click(function(){
+        $(".dealtype span").each(function(){
+            $(this).removeClass("active")
+        });
+
+        $(".showEncash").addClass("active");
+        $(".tbody1").hide();
+        $(".tbody2").hide();
+        $(".tbody4").hide();
+        $(".tbody5").hide();
+        $(".tbody3").show();
+    });
+    $(".showInvest").click(function(){
+        $(".dealtype span").each(function(){
+            $(this).removeClass("active")
+        });
+
+        $(".showInvest").addClass("active");
+        $(".tbody1").hide();
+        $(".tbody2").hide();
+        $(".tbody3").hide();
+        $(".tbody5").hide();
+        $(".tbody4").show();
+    });
+    $(".showGain").click(function(){
+        $(".dealtype span").each(function(){
+            $(this).removeClass("active")
+        });
+
+        $(".showGain").addClass("active");
+        $(".tbody1").hide();
+        $(".tbody2").hide();
+        $(".tbody3").hide();
+        $(".tbody4").hide();
+        $(".tbody5").show();
+    })
 </script>
